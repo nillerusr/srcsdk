c62f93e6895328d7885f61e59daebecb357798f6
a1batross <a1ba.omarov@gmail.com>
2016-08-23 15:32:02 +0600

    Try use GCC 4.9

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-make -f games.mak $1 NDK=1 NDK_PATH=/home/a1ba/.android/android-ndk-r10e APP_API_LEVEL=21 -j4
+export NDK_TOOLCHAIN_VERSION=4.9
+make -f $2.mak $1 NDK=1 NDK_PATH=/home/a1ba/.android/android-ndk-r10e APP_API_LEVEL=19 CFG=debug -j4
