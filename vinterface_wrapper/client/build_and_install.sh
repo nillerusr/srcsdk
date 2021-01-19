b1be9fbf121afaf486137fd8ca89f7588b03e802
nillerusr <nillerusr@gmail.com>
2021-01-19 22:23:58 +0000

    test

--- vinterface_wrapper/client/build_and_install.sh
+++
@@ -1,2 +0,0 @@
-source ~/.profile
-make clean && amake2 -j5 && cp libclient.so ~/projects/srceng-android/libs/armeabi-v7a/ && cp libclient.so /mnt/data/app/com.nvidia.valvesoftware.halflife2*/lib/arm/ && echo lib installed
